e0118596285a946144772917f12468f6757ef6d2
sfoster <sam@sam-i-am.com>
2012-08-30 15:02:25 +0100

    Adding a just-AMD, unminified output using minimal define(function(){ ... }) boilerplate

--- build/output/knockout-latest.amd.js
+++ build/output/knockout-latest.amd.js
@@ -2,23 +2,7 @@
 // (c) Steven Sanderson - http://knockoutjs.com/
 // License: MIT (http://www.opensource.org/licenses/mit-license.php)
 
-(function(){
-var DEBUG=true;
-(function(window,document,navigator,jQuery,undefined){
-!function(factory) {
-    // Support three module loading scenarios
-    if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
-        // [1] CommonJS/Node.js
-        var target = module['exports'] || exports; // module.exports is for Node.js
-        factory(target);
-    } else if (typeof define === 'function' && define['amd']) {
-        // [2] AMD anonymous module
-        define(['exports'], factory);
-    } else {
-        // [3] No module loader (plain <script> tag) - put directly in global namespace
-        factory(window['ko'] = {});
-    }
-}(function(koExports){
+define(function(){
 // Internally, all KO objects are attached to koExports (even the non-exported ones whose names will be minified by the closure compiler).
 // In the future, the following "ko" variable may be made distinct from "koExports" so that private objects are not externally reachable.
 var ko = typeof koExports !== 'undefined' ? koExports : {};
@@ -3543,6 +3527,6 @@ ko.exportSymbol('nativeTemplateEngine', ko.nativeTemplateEngine);
 
     ko.exportSymbol('jqueryTmplTemplateEngine', ko.jqueryTmplTemplateEngine);
 })();
+// AMD boilerplate
+return ko;
 });
-})(window,document,navigator,window["jQuery"]);
-})();
