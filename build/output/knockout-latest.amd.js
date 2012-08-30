f170b41349cd2273930c3e2d66698497ca8420ef
sfoster <sam@sam-i-am.com>
2012-08-30 15:06:07 +0100

    Add the DEBUG flag back in for AMD output

--- build/output/knockout-latest.amd.js
+++ build/output/knockout-latest.amd.js
@@ -3,6 +3,7 @@
 // License: MIT (http://www.opensource.org/licenses/mit-license.php)
 
 define(function(){
+var DEBUG=true;
 // Internally, all KO objects are attached to koExports (even the non-exported ones whose names will be minified by the closure compiler).
 // In the future, the following "ko" variable may be made distinct from "koExports" so that private objects are not externally reachable.
 var ko = typeof koExports !== 'undefined' ? koExports : {};
